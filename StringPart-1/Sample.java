bb2533bb4bd48c04e3b6c3fc39648de3e7df4555
kalpesh <kalpesh162@gmail.com>
2026-07-09 10:13:36 +0530

    String Part-1

---
+++ StringPart-1/Sample.java
@@ -0,0 +1,32 @@
+/*class Sample{
+	public static void main(String[] args) {
+		String s1="Kareena";
+		s1.concat("Kapoor");
+		System.out.println(s1);
+	}
+}
+public String concat(String){
+	 return
+}
+*/
+/*
+class Sample{
+	public static void main(String[] args) {
+		String s1="Kareena";
+		s1=s1.concat("Kapoor");
+		System.out.println(s1);
+	}
+}
+*/
+
+class Sample{
+	public static void main(String[] args) {
+		String s1="Kareena";
+		String s3=s1.concat("Kapoor");
+		//String s4=s1+"Kapoor";
+		System.out.println(s1);
+		System.out.println(s3);
+	}
+}
+
+
